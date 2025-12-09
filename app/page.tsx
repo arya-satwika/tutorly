e56e3b0b444f280c67210ca2d7b496708af4f227
arya-satwika <aryastwk95@gmail.com>
2025-12-09 23:14:25 +0700

    tarok navbar di layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,8 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <Navbar />
       <div className="flex-1 bg-black p-4">
         <h1 className="text-white">Welcome to Tutorly!</h1>
       </div>
-    </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,37 @@
 import { getUserById } from "@/lib/db/queries"
 import { dummyUser } from "@/lib/db/dummy-data";
 
-export default async function Navbar() {
+export default async function Navbar({ children }: { children: React.ReactNode }) {
 
   const currentUser = dummyUser; // masukkinn id user ke parameter
   // const currentUser = await getUserById(1); // masukkinn id user ke parameter
   return (
-    <div className="flex flex-col min-h-screen py-3 max-w-40 px-5 items-top justify-between bg-gray-800 font-sans dark:bg-white">
-      <div className="flex flex-col items-center gap-2">
-        <h1 className="text-3xl font-bold text-zinc-200 dark:text-blue-800">
-            Tutorly
-        </h1>
-        <div>
-          <h1 className="text-gray-800 text-xl font-bold">{currentUser.name}</h1>
-          <h2 className="text-gray-500 text-sm">{currentUser.prodi}</h2>
+    <div className="flex">
+      <div className="flex flex-col min-h-screen py-3 max-w-40 px-5 items-top justify-between bg-gray-800 font-sans dark:bg-white">
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-3xl font-bold text-zinc-200 dark:text-blue-800">
+              Tutorly
+          </h1>
+          <div>
+            <h1 className="text-gray-800 text-xl font-bold">{currentUser.name}</h1>
+            <h2 className="text-gray-500 text-sm">{currentUser.prodi}</h2>
+          </div>
+          <h1 className="text-xl text-zinc-200 dark:text-gray-800">
+              Profile
+          </h1>
+          <h1 className="text-xl text-zinc-200 dark:text-gray-800">
+              Settings
+          </h1>
+          <h1 className="text-xl text-zinc-200 dark:text-gray-800">
+              My Courses
+          </h1>
         </div>
-        <h1 className="text-xl text-zinc-200 dark:text-gray-800">
-            Profile
-        </h1>
-        <h1 className="text-xl text-zinc-200 dark:text-gray-800">
-            Settings
-        </h1>
-        <h1 className="text-xl text-zinc-200 dark:text-gray-800">
-            My Courses
-        </h1>
+        <div>
+          <h1 className="text-black">test</h1>
+        </div>
       </div>
-      <div>
-        <h1 className="text-black">test</h1>
+      <div className="flex-1">
+      { children }
       </div>
     </div>
   );
