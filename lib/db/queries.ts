10f915e92e094e778fa4ab7b97ae0a6370cf0948
arya-satwika <aryastwk95@gmail.com>
2025-12-15 22:38:48 +0700

    refactor fungsi login & register  page

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -2,7 +2,7 @@ import { users } from '@/lib/db/schema';
 import { db } from '@/lib/db/index';
 import { eq } from 'drizzle-orm';
 import { DrizzleQueryError } from 'drizzle-orm/errors';
-import { DatabaseError } from 'pg'; // or your DB driver
+import { DatabaseError } from 'pg';
 
 export type insertUserType = typeof users.$inferInsert;
 
@@ -15,11 +15,11 @@ export const insertUser = async ( thisUser: insertUserType ) => {
     }catch (error) {
          if (error instanceof DrizzleQueryError) {
             const dbError = error.cause as DatabaseError;
-            
             console.log('PostgreSQL Error Code:', dbError.code);
             console.log('Error Message:', dbError.message);
-            console.log('Detail:', dbError.detail);
-            console.log('Constraint:', dbError.constraint);
+            if (dbError.code === '23505') { // unique_violation
+                return { succes: false, message: "username already exists" };
+            }
          }
         return { succes: false, message: "Error inserting user: " + error as string };
     }
@@ -36,21 +36,23 @@ export const getUserById = async (id: number) => {
 
 export const getUserByName = async (name: string, password: string) => {
     try {
-        const user = await db
+        const [user] = await db
             .select()
             .from(users)
             .where(eq(users.name, name))    
             .limit(1);
-        if(user.length > 0) {
-            if (user[0].password === password) {
-                return { success: true, message: "User found", currentUser: user[0] };
+        if(user) {
+            if (user.password === password) {
+                return { succes: true, message: "User found", currentUser: user };
+            } else {
+                return { succes: false, message: "Incorrect password", currentUser: null };
             }
         }   
     } catch (error) {
         console.error("Error fetching user by name:", error);
-        return { success: false, message: "Error fetching user by name: " + error as string, currentUser: null };
+        return { succes: false, message: "Error fetching user by name: " + error as string, currentUser: null };
     }
-    return { success: false, message: "User not found or incorrect password", currentUser: null };
+    return { succes: false, message: "User not found or incorrect password", currentUser: null };
 
 }
 
